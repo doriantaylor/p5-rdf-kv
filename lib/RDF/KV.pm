ef6aba6e6b91ba4979d2b335ac16dfba32435528
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-10-05 11:00:37 -0700

    initial import of RDF::KV

---
+++ lib/RDF/KV.pm
@@ -0,0 +1,117 @@
+package RDF::KV;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RDF::KV - The great new RDF::KV!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RDF::KV;
+
+    my $foo = RDF::KV->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dorian Taylor, C<< <dorian at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rdf-kv at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RDF-KV>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RDF::KV
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RDF-KV>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RDF-KV>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RDF-KV>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RDF-KV/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Dorian Taylor.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+=cut
+
+1; # End of RDF::KV
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'RDF::KV' ) || print "Bail out!\n";
+}
+
+diag( "Testing RDF::KV $RDF::KV::VERSION, Perl $], $^X" );
