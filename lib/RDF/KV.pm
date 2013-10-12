65dd107d85a572a34794c150b08a3bf8e2ff5181
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-10-12 13:24:00 -0700

    splitting apart param massage from deref operation

--- lib/RDF/KV.pm
+++ lib/RDF/KV.pm
@@ -7,6 +7,7 @@ use warnings FATAL => 'all';
 # might as well use full-blown moose if URI::NamespaceMap uses it
 use Moose;
 use namespace::autoclean;
+use Try::Tiny;
 
 use Carp         ();
 use Scalar::Util ();
@@ -30,34 +31,31 @@ our $VERSION = '0.01';
 
 # here's ye olde grammar:
 
-my $MODIFIER = qr/(?:[+-]!|![+-]|!)/o;
-my $PLACEHOLDER = qr/#$XML::RegExp::NCName/o;
-my $TERM = qr/(?:$PLACEHOLDER|
-                  (?:$XML::RegExp::NCName|[A-Za-z][0-9A-Za-z.+-]*)
-                  :[^[:space:]]*)/xo;
-my $RFC5646 = qr/(?:[A-Za-z]+(?:-[0-9A-Za-z]+)*)/o;
-my $SIMPLE_DESIGNATOR = qr/[:_'@^]/o;
-my $ATOMIC_DESIGNATOR = qr/[:_'#]/o;
-#my $DECL_DESIGNATOR = qr/
-my $DESIGNATOR   = qr/(?:$ATOMIC_DESIGNATOR|\@$RFC5646|\^$TERM)/o;
-my $PARTIAL_STMT = qr/(?:($MODIFIER)\s+)?
-                      (?:($TERM)(?:\s+($TERM))?(?:\s+($DESIGNATOR))?|
-                          ($TERM)\s+($DESIGNATOR)\s+($TERM)|
-                          ($TERM)\s+($TERM)(?:\s+($DESIGNATOR))?\s+($TERM))/xo;
-my $LOCAL_DECL  = qr/($SIMPLE_DESIGNATOR)\s+($PLACEHOLDER)/o;
-my $GLOBAL_DECL = qr/(\$$XML::RegExp::NCName)/o;
-my $DECLARATION = qr/(?:$LOCAL_DECL|$GLOBAL_DECL)/o;
-
-my $RDFKV = qr/^\s*(?:$DECLARATION|$PARTIAL_STMT)\s*$/o;
-
-# connect regex positions to semantics
-my @MAP = qw(designator pdecl gdecl modifier term1 term2 designator
-             term1 designator graph term1 term2 designator graph);
-
 # XXX I know I said in the spec that the protocol should be parseable
 # by a single regex, but regexes make for lame, all-or-nothing
 # parsers. As such, this should really be rewritten when there's time
 # to create a more helpful (in the error message sense) parser.
+
+# ok you know what? no. This is waaaaaaay simpler.
+my $MODIFIER     = qr/(?:[=+-]!|![=+-]|!)/o;
+my $PREFIX       = qr/(?:$XML::RegExp::NCName|[A-Za-z][0-9A-Za-z.+-]*)/o;
+my $TERM         = qr/(?:$PREFIX:\S*)/o;
+my $RFC5646      = qr/(?:[A-Za-z]+(?:-[0-9A-Za-z]+)*)/o;
+my $DESIGNATOR   = qr/(?:[:_']|\@$RFC5646|\^$TERM)/o;
+my $DECLARATION  = qr/^\s*\$\s+($XML::RegExp::NCName)(?:\s+(\$))?\s*$/mo;
+my $MACRO        = qr/(?:\$\{($XML::RegExp::NCName)\}|
+                          \$($XML::RegExp::NCName))/xo;
+my $NOT_MACRO    = qr/(?:(?!\$$XML::RegExp::NCName|
+                              \$\{$XML::RegExp::NCName\}).)*/xo;
+my $MACROS       = qr/($NOT_MACRO)(?:$MACRO)?($NOT_MACRO)/smo;
+my $PARTIAL_STMT = qr/^\s*(?:($MODIFIER)\s+)?
+                      (?:($TERM)(?:\s+($TERM))?(?:\s+($DESIGNATOR))?|
+                          ($TERM)\s+($DESIGNATOR)\s+($TERM)|
+                          ($TERM)\s+($TERM)(?:\s+($DESIGNATOR))?\s+($TERM))
+                      (?:\s+(\$))\s*$/xmo;
+
+my @MAP = qw(modifier term1 term2 designator term1 designator graph
+             term1 term2 designator graph deref);
 
 =head1 SYNOPSIS
 
@@ -82,15 +80,20 @@ my @MAP = qw(designator pdecl gdecl modifier term1 term2 designator
 =cut
 
 has subject => (
-    is => 'rw',
+    is       => 'rw',
+    required => 1,
 );
 
 has graph => (
-    is => 'rw',
+    is  => 'rw',
+    isa => 'RDF::Trine::Model',
+    
 );
 
 has namespaces => (
-    is => 'ro',
+    is      => 'ro',
+    isa     => 'URI::NamespaceMap',
+    default => sub { URI::NamespaceMap->new },
 );
 
 has _placeholders => (
@@ -108,25 +111,6 @@ has _statements => (
 
 =cut
 
-sub _parse_key {
-    my ($self, $k) = @_;
-    my @x = ($k =~ $RDFKV);
-
-    # XXX currently this "parser" either matches the protocol or
-    # doesn't match anything at all. It would be better if it would
-    # ignore keys that clearly weren't part of the protocol, while
-    # raising exceptions for malformed attempts to adhere to it.
-    return unless @x;
-
-    # kinda important, no?
-    die 'INTERNAL ERROR: Map no longer matches grammar.' unless @x == @MAP;
-
-    my %x;
-    map { $x{$MAP[$_]} = $x[$_] if defined $x[$_]  } (0..$#x);
-
-    return \%x;
-}
-
 # variants of parameter getters
 
 sub _1 {
@@ -140,22 +124,234 @@ sub _2 {
     ref $val ? @$val : $val;
 }
 
-my %GLOBALS = (
+# XXX these should all get syntax checks/CURIE expansion/etc
+my %SPECIALS = (
     SUBJECT => sub {
         my ($self, $val) = @_;
-        $self->subject($val);
+        $self->subject($val->[-1]) if @$val;
     },
     GRAPH => sub {
         my ($self, $val) = @_;
-        $self->graph($val);
+        $self->graph($val->[-1]) if @$val;
     },
     PREFIX => sub {
         my ($self, $val) = @_;
         # XXX CHECK THIS MUTHA
-        my ($prefix, $uri) = split /\s+/, $val;
-        $self->namespaces->add_mapping($prefix, $uri);
+        for my $v (@$val) {
+            my ($prefix, $uri) = ($v =~ /^\s*(\S+):\s+(.*)$/)
+                or Carp::croak("Invalid prefix mapping $val");
+            $self->namespaces->add_mapping($prefix, $uri);
+        }
     },
 );
+
+sub _deref_list {
+    my ($vals, $macros) = @_;
+    my @out;
+
+    for my $v (@$vals) {
+        # make this versatile
+        $v = $v->[0] if ref $v;
+
+        my @chunks;
+        while ($v =~ /\G$MACROS/gco) {
+            my $pre   = $1;
+            my $macro = $2 || $3;
+            my $post  = $4;
+
+            unless (defined $macro) {
+                @chunks = ($pre . $post);
+                #warn @chunks;
+                next;
+            }
+
+            # do the actual macro dereferencing or noop in
+            # lieu of a bound macro
+            my @x = $macros->{$macro} ?
+                map { "$pre$_$post" } @{$macros->{$macro}}
+                    : ("$pre$macro$post");
+
+            # initialize chunks
+            unless (@chunks) {
+                @chunks = @x;
+                next;
+            }
+
+            # replace chunks with product of itself and x
+            my @y;
+            for my $c (@chunks) {
+                for my $d (@x) {
+                    push @y, "$c$d";
+                }
+            }
+            @chunks = @y;
+        }
+
+        push @out, @chunks;
+    }
+
+    wantarray ? @out : \@out;
+}
+
+sub _massage_macros {
+    my $macros = shift;
+    # XXX this currently makes destructive changes to $macros insofar
+    # as it rewrites the 'deref' flag with the actual variables to
+    # dereference, or to 0 if there aren't any. If this becomes a
+    # problem, just use Clone.
+
+    # cycle detect, finished product
+    my (%seen, %done);
+
+    # shallow-copy the hash
+    my %pending = %$macros;
+
+    # Start a queue with a (quasi) random macro.
+    my @queue = (keys %pending)[0];
+
+    # If none of them contain a (bound) macro references, that macro
+    # is 'done'.
+
+    # If the values *do* contain bound macro references, check to see
+    # if those are 'done'. If they aren't, *prepend* the keys to the
+    # queue, before the current macro.
+
+    while (@queue) {
+        #warn 'Initial ' . join(';', @queue);
+        my $k = shift @queue;
+        #warn "beginning \$$k";
+
+        $seen{$k}++;
+
+        my @vals = @{$macros->{$k}};
+
+        # 'done' and 'pending' macros
+        my (%dm, %pm);
+
+        # Examine each of its values.
+
+        # Note: this test is equivalent to concatenating the values
+        # together with spaces and performing the regex on that. But
+        # we can't do that because we're storing the macro-matching
+        # state of individual values.
+        for my $pair (@vals) {
+            my ($val, $deref) = @$pair;
+
+            # no expando
+            next unless $deref;
+
+            if (ref $deref) {
+                # already been scanned
+                for my $m (@$deref) {
+                    defined $done{$m} ? $dm{$m}++ : $pm{$m}++;
+                }
+            }
+            else {
+                my %m;
+                for my $m (grep { defined $_ } ($val =~ /$MACRO/og)) {
+
+                    # check first to see if it's bound
+                    next unless $macros->{$m};
+                    #warn $m;
+
+                    # if it's yourself, explode
+                    Carp::croak("Self reference found!") if $m eq $k;
+
+                    # get this to replace deref
+                    $m{$m}++;
+
+                    # and get this to figure out if we can deref
+                    defined $done{$m} ? $dm{$m}++ : $pm{$m}++;
+                }
+
+                # now replace deref
+                $pair->[1] = keys %m ? [sort keys %m] : 0;
+            }
+        }
+
+        # macro values have pending matches
+        if (keys %pm) {
+            # this is where we would detect a cycle
+
+            # right HERE
+
+            my @q;
+            for my $m (keys %pm) {
+                Carp::croak("Cycle detected between $k and $m") if $seen{$m};
+                push @q, $m;
+            }
+            #warn join '/', @q;
+
+            # do it again
+            unshift @queue, @q, $k;
+            #warn join ',', @queue;
+
+            next;
+        }
+        elsif (keys %dm) {
+            # macro values have actionable matches
+
+            #warn "replacing values for \$$k";
+
+            # replace contents and mark done
+            my @out;
+            for my $vv (@vals) {
+                my ($v, $deref) = @$vv;
+
+                my @chunks;
+                while ($v =~ /\G$MACROS/gco) {
+                    my $pre   = $1;
+                    my $macro = $2 || $3;
+                    my $post  = $4;
+
+                    unless (defined $macro) {
+                        @chunks = ($pre . $post);
+                        #warn @chunks;
+                        next;
+                    }
+
+                    # do the actual macro dereferencing or noop in
+                    # lieu of a bound macro
+                    my @x = $done{$macro} ?
+                        map { "$pre$_$post" } @{$done{$macro}}
+                            : ("$pre$macro$post");
+
+                    # initialize chunks
+                    unless (@chunks) {
+                        @chunks = @x;
+                        next;
+                    }
+
+                    # replace chunks with product of itself and x
+                    my @y;
+                    for my $c (@chunks) {
+                        for my $d (@x) {
+                            push @y, "$c$d";
+                        }
+                    }
+                    @chunks = @y;
+                }
+
+                push @out, @chunks;
+            }
+
+            $done{$k} = \@out;
+        }
+        else {
+            #warn Data::Dumper::Dumper(\@vals);
+            # nothing to do, mark done
+            $done{$k} = [map { $_->[0] } @vals];
+        }
+
+        # remember to remove this guy or we'll loop forever
+        delete $pending{$k};
+
+        # replenish the queue with another pending object
+        push @queue, (keys %pending)[0] if !@queue and keys %pending;
+    }
+
+    \%done;
+}
 
 
 sub process {
@@ -165,65 +361,90 @@ sub process {
     my $sub = Scalar::Util::blessed($params)
         && $params->can('get_all') ? \&_1 : \&_2;
 
-    my (%globals, %placeholders, @statements);
+    my (%macros, %maybe, %neither);
 
-    # run over the parsed parameters
     for my $k (keys %$params) {
-        my $rec = $self->_parse_key($k) or next;
+        # Step 0: get the values into a homogeneous list
+        my @v = $sub->($params, $k);
 
-        my @v   = $sub->($params, $k);
+        # Step 1: pull out all the macro declarations
+        if (my ($name, $sigil) = ($k =~ $DECLARATION)) {
+            # Step 1.0.1: create [content, deref flag] pairs
 
-        if (defined $rec->{gdecl}) {
-            my $y = $globals{$rec->{gdecl}} ||= [];
-            push @$y, @v;
+            # OOH VERY CLEVER
+            push @{$macros{$name} ||= []}, (map { [$_, int(!!$sigil)] } @v);
         }
-        elsif (defined $rec->{pdecl}) {
-            my $y = $placeholders{$rec->{pdecl}} ||= {};
-            my $z = $y->{$rec->{designator} || "'"} ||= [];
-            push @$z, @v;
+        # Step 1.1: set aside candidate statements
+        elsif ($k =~ /^\s*\S+\s+\S+.*?/ || $k =~ /[:$]/) {
+            # valid partial statements will contain space or : or $
+            push @{$maybe{$k} ||= []}, @v;
         }
-        elsif (defined $rec->{term1}) {
-            if (defined $rec->{modifier}) {
-                $rec->{modifier} = {
-                    map { $_ => 1 } (split //, $rec->{modifier}) };
-            }
-            else {
-                $rec->{modifier} = {};
-            }
-
-            # now deal with designator
-            if (defined $rec->{designator}) {
-                my ($a, $b) = ($rec->{designator} =~ /^(.)(.*?)$/);
-                if (defined $b and $b ne '') {
-                    $rec->{designator} = $a;
-                    if ($a eq '@') {
-                        $rec->{lang} = $b;
-                    }
-                    elsif ($a eq '^') {
-                        $rec->{datatype} = $b;
-                    }
-                    else {
-                        die "INTERNAL ERROR: Unknown designator $a";
-                    }
-                }
-            }
-
-            push @statements, [$rec, \@v];
-        }
+        # Step 1.2: put the rest in a discard pile
         else {
-            require Data::Dumper;
-            die 'INTERNAL ERROR: Unrecognized parameter: ' .
-                Data::Dumper::Dumper($rec);
+            push @{$neither{$k} ||= []}, @v;
         }
     }
 
-    return [\%globals, \%placeholders, \@statements];
+    # cycles should cause a 409 Conflict error, but that isn't our job
+    # here.
 
-    # run over globals
+    # XXX although it may be useful to return an object in $@ that was
+    # more informative.
 
-    # 
+    # Step 2: dereference all the macros (that asked to be)
+    try {
+        my $x = _massage_macros(\%macros);
+        %macros = %$x;
+    } catch {
+        # move this error up in the stack
+        Carp::croak($@);
+    };
+
+    # Step 2.1: overwrite any reserved/magic macros
+    $macros{NEWUUID} = [[sub {  }, 0]];
+    # XXX make this extensible?
+
+    # Step 3: apply special control macros to $self
+    try {
+        for my $k (keys %SPECIALS) {
+            next unless $macros{$k};
+            $SPECIALS{$k}->($self, $macros{$k});
+        }
+    } catch {
+        # cough any errors up the stack
+        Carp::croak($@);
+    };
+
+    # Step 4: dereference macros in statements
+
+    # Step 4.1 dereference macros in statement *templates* first so we
+    # can figure out which values need to be dereferenced (since the
+    # terminating $ indicator can be substituted in via macro).
+
+    # in this case we probably can't be clever and reuse the values
+    # for multiple templates because some may or may not include the
+    # indicator.
+
+    # actually we can reuse the values, we just can't parse them until
+    # we've parsed the statement templates, because those tell us what
+    # to do with the values.
+
+    # which also means we have to parse the statement templates
+    # immediately.
+
+    # there is still the issue of the empty string: what does it mean,
+    # and in what context?
+
+    # Step 4.2 dereference macros in statement *values* (that asked to be)
+
+
+    # Step 5: parse statement templates
+
+    # Step 5.1 expand qnames
+
+    # Step 6: generate complete statements
+
 }
-
 
 =head1 AUTHOR
 
